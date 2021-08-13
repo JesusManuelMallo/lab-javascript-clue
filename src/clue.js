35fdc849b93f8e8ffcd10625b96e40c0cf8d07d6
JesusManuelMallo <88166930+JesusManuelMallo@users.noreply.github.com>
2021-08-13 13:24:05 +0200

    Update clue.js

--- src/clue.js
+++ src/clue.js
@@ -2,15 +2,133 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+  {
+    firstName: 'Jesus',
+    lastName: 'Mallo',
+    occupation: 'Estudiante'
+    
+  },
+  {
+    firstName: 'Sandra'
+    lastName: 'Bosk'
+    occupation: 'jefa'
+  },
+  {
+    firstName: 'Curro'
+    lastName: 'Jimenez'
+    occupation: 'profe'
+  },
+  {
+    firstName: 'Salava'
+    lastName: 'Garcia'
+    occupation: 'asistente'
+  },
+  {
+    firstName: 'Alex'
+    lastName: 'Escofet'
+    occupation: 'segundo asistente'
+  },
+ 
+
+];
+
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  {
+    room1: 'comedor'
+  },
+  {
+    room2: 'cocina'
+  },
+  {
+    room3: 'lavabo'
+  },
+  {
+    room4: 'sala de billar'
+  },
+  {
+    room5: 'dormitorio grande'
+  },
+  {
+    room6: 'sala de juegos'
+  },
+  {
+    room7: 'habitación de invitados'
+  },
+  {
+    room8: 'habitación 8'
+  },
+
+  {
+    room9: 'habitación 9'
+  },
+
+  {
+    room10: 'habitación 10'
+  },
+
+
+  {
+    room11: 'habitación 11'
+  },
+
+
+  {
+    room12: 'habitación 12'
+  },
+
+
+  {
+    room13: 'habitación 13'
+  },
+
+
+  {
+    room14: 'habitación 14'
+  },
+
+
+  {
+    room15: 'habitación 15'
+  },
+
+
+
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {
+    name: 'pistol'
+    weigth: '20'
+  },
+
+  {
+    name: 'pistol'
+    weigth: '20'
+  },
+
+  {
+    name: 'pistol'
+    weigth: '20'
+  },
+
+  {
+    name: 'pistol'
+    weigth: '20'
+  },
+
+  {
+    name: 'pistol'
+    weigth: '20'
+  },
+
+
+];
 
 
 // ITERATION 2
